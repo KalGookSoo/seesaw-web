0937a04d7011c0e9b5c88585050287f4ece4b330
kimdoyeob <ga.miro3721@gmail.com>
2025-03-25 09:40:52 +0900

    feat: 상세페이지 퍼블리싱파일 추가

---
+++ src/main/java/at/modoo/controller/DemoController.java
@@ -0,0 +1,39 @@
+package at.modoo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/demo")
+@Controller
+public class DemoController {
+
+    @GetMapping("/{viewName}")
+    public String demo(@PathVariable("viewName") String viewName) {
+        return "demo/" + viewName;
+    }
+
+/*
+    @GetMapping("/one")
+    public String demo1() {
+        return "demo/1";
+    }
+*/
+
+/*
+    @GetMapping("/two")
+    public String demo2() {
+        return "demo/2";
+    }
+*/
+
+/*
+    @GetMapping("/three")
+    public String demo3() {
+        return "demo/3";
+    }
+*/
+
+}
