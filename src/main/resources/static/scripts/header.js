9b586b429b69ec101b336e93dc637d7d6f32eab6
kimdoyeob <ga.miro3721@gmail.com>
2025-04-08 17:48:42 +0900

    fix: 메인페이지를 제외한 헤더 ux 변경

--- src/main/resources/static/scripts/header.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', e => {
-    const header = e.target.getElementById('header');
-    e.target.addEventListener('scroll', e => {
-        const methodName = window.scrollY > 0 ? 'add' : 'remove';
-        header.classList[methodName]('scrolled');
-    });
-});
