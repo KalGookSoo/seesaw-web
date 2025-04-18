6b217c3c68a83eba26bce12b26a0e57e78b6ecb6
kimdoyeob <ga.miro3721@gmail.com>
2025-04-18 16:12:30 +0900

    feat: 게시글 수정 기능 최적화 수정 시 기존에 있던 이미지를 제거하였을 경우 첨부파일 데이터와 파일을 삭제하는 로직 추가, 이미지를 새로 추가할 경우 새로 데이터 삽입 후 파일 쓰기 기능 추가

--- src/main/java/at/modoo/service/DefaultArticleService.java
+++ src/main/java/at/modoo/service/DefaultArticleService.java
@@ -7,10 +7,12 @@ import at.modoo.core.file.FileIOService;
 import at.modoo.model.*;
 import at.modoo.repository.*;
 import at.modoo.search.ArticleSearch;
+import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpServletRequest;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -44,6 +46,8 @@ public class DefaultArticleService implements ArticleService {
 
     private final PrincipalProvider principalProvider;
 
+    private final EntityManager entityManager;
+
     public DefaultArticleService(
             @Value("${at.modoo.filepath}") String filepath,
             ArticleRepository articleRepository,
@@ -51,7 +55,8 @@ public class DefaultArticleService implements ArticleService {
             ReplyRepository replyRepository,
             ViewRepository viewRepository,
             AttachmentRepository attachmentRepository,
-            PrincipalProvider principalProvider
+            PrincipalProvider principalProvider,
+            EntityManager entityManager
     ) {
         this.filepath = filepath;
         this.articleRepository = articleRepository;
@@ -60,6 +65,7 @@ public class DefaultArticleService implements ArticleService {
         this.viewRepository = viewRepository;
         this.attachmentRepository = attachmentRepository;
         this.principalProvider = principalProvider;
+        this.entityManager = entityManager;
     }
 
     @Transactional(readOnly = true)
@@ -115,6 +121,8 @@ public class DefaultArticleService implements ArticleService {
 
     @Override
     public Article create(CreateArticleCommand command) throws IOException {
+
+        // 생성될 게시글의 식별자를 참조하기위해 먼저 게시글을 저장한다.
         Article article = Article.create(command);
         articleRepository.save(article);
 
@@ -148,6 +156,7 @@ public class DefaultArticleService implements ArticleService {
             attachmentRepository.save(attachment);
         }
 
+        // 인라인이미지 링크를 첨부파일 API로 치환한 본문으로 재할당한다
         article.setContent(document.body().html());
         articleRepository.save(article);
         return article;
@@ -156,6 +165,53 @@ public class DefaultArticleService implements ArticleService {
     @Override
     public Article update(String id, UpdateArticleCommand command) throws IOException {
         Article article = articleRepository.getReferenceById(id);
+
+        Document existingContent = Jsoup.parse(article.getContent());
+        Elements existingImages = existingContent.select("img[src*=\"/api/attachments/\"]");
+        
+        List<String> deletedAttachmentIds = new ArrayList<>();
+        
+        Document newContent = Jsoup.parse(command.getContent());
+        Elements newImages = newContent.select("img[src*=\"/api/attachments/\"]");
+
+        // src는 "/"로 스플릿하여 마지막 요소를 uuid4 패턴의 문자열이다.
+        for (Element existingImage : existingImages) {
+            String existingSrc = existingImage.attr("src");
+            boolean isPresentInNewImages = newImages.stream()
+                    .anyMatch(newImage -> newImage.attr("src").equals(existingSrc));
+            if (!isPresentInNewImages) {
+                deletedAttachmentIds.add(existingSrc.substring(existingSrc.lastIndexOf("/") + 1));
+            }
+        }
+
+        // 수정하면서 삭제한 이미지를 삭제
+        List<Attachment> attachments = attachmentRepository.findAllById(deletedAttachmentIds);
+        attachmentRepository.deleteAllInBatch(attachments);
+        attachments.stream().map(attachment -> filepath + attachment.getPathName() + File.separator + attachment.getName()).forEach(FileIOService::delete);
+
+        Iterator<Element> iterator = newContent.select("img[src*=\"blob:\"]").iterator();
+
+        for (MultipartFile multipartFile : command.getInlineImages()) {
+            Attachment attachment = Attachment.create(article.getId(), Attachment.Type.INLINE_IMAGE, multipartFile);
+            writeFile(filepath + attachment.getPathName() + File.separator + attachment.getName(), multipartFile.getBytes());
+            attachmentRepository.save(attachment);
+
+            // request에서 host 주소 추출
+            // TODO 이거 인터페이스 분리하여 외부에서 의존관계 주입받아야 함
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+            HttpServletRequest request = attributes.getRequest();
+            String host = request.getHeader("host");
+            String scheme = request.getScheme();
+            String url = scheme + "://" + host + "/api/attachments/" + attachment.getId();
+
+            // images의 src를 첨부파일을 생성 후 "/api/attachments/{id}"로 치환한다.
+            if (iterator.hasNext()) {
+                Element element = iterator.next();
+                element.attr("src", url);
+            }
+        }
+
+        command.setContent(newContent.body().html());
         article.update(command);
         articleRepository.save(article);
 
