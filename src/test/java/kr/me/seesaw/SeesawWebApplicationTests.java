2f2db47213588cf5eb79bd5b1755567002456a0c
kimdoyeob <ga.miro3721@gmail.com>
2025-06-19 22:16:22 +0900

    feat: 워크플로우 단위 테스트 추가 및 빈 지연 초기화 옵션 추가

--- src/test/java/kr/me/seesaw/SeesawWebApplicationTests.java
+++ src/test/java/kr/me/seesaw/SeesawWebApplicationTests.java
@@ -1,12 +1,11 @@
 package kr.me.seesaw;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SeesawWebApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
