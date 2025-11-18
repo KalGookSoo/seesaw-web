ac34be64c45d544a7747cba388bb3ef8581ec4c1
Doyevskyi <ga.miro3721@gmail.com>
2025-11-18 19:34:32 +0900

    feat: ApplicationPidFileWriter 추가

--- src/main/java/kr/me/seesaw/SeesawWebApplication.java
+++ src/main/java/kr/me/seesaw/SeesawWebApplication.java
@@ -2,12 +2,16 @@ package kr.me.seesaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SeesawWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeesawWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SeesawWebApplication.class);
+        ApplicationPidFileWriter writer = new ApplicationPidFileWriter();
+        application.addListeners(writer);
+        application.run(args);
+    }
 
 }
