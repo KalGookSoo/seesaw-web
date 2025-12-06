cc4a34ace7076ac7cce584e325d96cc4353f819c
Doyevskyi <ga.miro3721@gmail.com>
2025-12-06 22:45:40 +0900

    fix: pid 쓰기 기능 수정

--- src/main/java/kr/me/seesaw/SeesawWebApplication.java
+++ src/main/java/kr/me/seesaw/SeesawWebApplication.java
@@ -2,12 +2,15 @@ package kr.me.seesaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SeesawWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeesawWebApplication.class, args);
+        SpringApplication application = new SpringApplication(SeesawWebApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
