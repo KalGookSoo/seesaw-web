f3922fcc86f531c6bfd7e0ec00da63119f66ec04
Doyevskyi <ga.miro3721@gmail.com>
2026-03-02 22:06:49 +0900

    feat: DefaultEventWebService에 이벤트 권한 처리 로직 추가
    - 이벤트 권한 및 소유권 확인을 관리하기 위해 EventPermissionService 인터페이스 및 구현체 도입
    - 보안 액세스 제어를 위해 PermissionEvaluator 및 PrincipalProvider 의존성 추가
    - 이벤트 관련 작업에 대한 인가(Authorization) 및 소유권 검증 로직 강화

--- src/main/java/kr/me/seesaw/service/DefaultEventWebService.java
+++ src/main/java/kr/me/seesaw/service/DefaultEventWebService.java
@@ -2,7 +2,9 @@ package kr.me.seesaw.service;
 
 import kr.me.seesaw.command.CreateArticleCommand;
 import kr.me.seesaw.command.UpdateArticleCommand;
+import kr.me.seesaw.core.authentication.PrincipalProvider;
 import kr.me.seesaw.domain.Article;
+import kr.me.seesaw.domain.Category;
 import kr.me.seesaw.domain.VEvent;
 import kr.me.seesaw.domain.vo.ArticleType;
 import kr.me.seesaw.dto.command.CreateEventCommand;
@@ -15,6 +17,9 @@ import kr.me.seesaw.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,11 +29,16 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-@Service
+@Transactional
 @RequiredArgsConstructor
-public class DefaultEventWebService implements EventWebService {
+@Service
+public class DefaultEventWebService implements EventWebService, EventPermissionService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final PrincipalProvider principalProvider;
+
+    private final PermissionEvaluator permissionEvaluator;
 
     private final EventRepository eventRepository;
 
@@ -147,6 +157,26 @@ public class DefaultEventWebService implements EventWebService {
         eventRepository.delete(event);
         articleService.delete(event.getArticleId());
     }
+
+    @Override
+    public boolean hasPermission(String eventId, BasePermission permission) {
+        logger.debug("인가를 검증합니다. eventId: {}, permission: {}", eventId, permission);
+        VEvent event = eventRepository.getReferenceById(eventId);
+        Article article = event.getArticle();
+        Category category = article.getCategory();
+        return permissionEvaluator.hasPermission(principalProvider.getAuthentication(), category.getId(), Category.class.getCanonicalName(), permission);
+    }
+
+    @Override
+    public boolean isOwner(String eventId) {
+        logger.info("이벤트 소유자 확인: eventId={}", eventId);
+        Authentication authentication = principalProvider.getAuthentication();
+        if (authentication == null) {
+            return false;
+        }
+        String username = authentication.getName();
+        VEvent event = eventRepository.getReferenceById(eventId);
+        return event.getCreatedBy().equals(username);
     }
 
 }
