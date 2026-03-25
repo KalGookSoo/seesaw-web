c4d1bfefcf93f00e127444153fd45764b03d2f02
Doyevskyi <ga.miro3721@gmail.com>
2026-03-25 09:44:52 +0900

    feat: 기본 미디어 타입을 text/html로 설정

--- src/main/java/kr/me/seesaw/controller/ArticleController.java
+++ src/main/java/kr/me/seesaw/controller/ArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +19,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/articles")
+@RequestMapping(value = "/articles", produces = MediaType.TEXT_HTML_VALUE)
 public class ArticleController {
 
     private final ArticleService articleService;
